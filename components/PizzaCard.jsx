34bd2461e7ac04ba443af4b46f10219f501feb64
Kanem <kanemdeng@email.com>
2022-10-14 10:11:46 -0700

    able to load data from mongoDB

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,14 +1,17 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/PizzaCard.module.css'
 
-const PizzaCard = () => {
+const PizzaCard = ({product}) => {
   return (
     <div className={styles.container}>
-      <Image src='/images/pizza.png' alt='' width='500' height='500' />
-      <h1 className={styles.title}>Pizza</h1>
-      <span className={styles.price}>$19.97</span>
+      <Link href={`/product/${product._id}`} >
+        <Image src={product.image} alt='' width='500' height='500' />
+      </Link>
+      <h1 className={styles.title}>{product.title}</h1>
+      <span className={styles.price}>${product.prices[0]}</span>
       <p className={styles.desc}>
-        Freah made Pizza
+        {product.desc}
       </p>
     </div>
   )
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -26,5 +26,5 @@ export default async function handler(req, res) {
       res.status(500).json(error.message)
     }
   }
-
 }
+
